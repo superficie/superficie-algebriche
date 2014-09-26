2d575f0c53de476b839e2df396fd79b98e44f296
Johan Commelin <j.m.commelin@gmail.com>
2014-09-26 12:08:53 +0200

    Add rational surfaces to the database of invariants

--- surflist.py
+++ surflist.py
@@ -6,16 +6,31 @@ import sqlite3
 conn = sqlite3.connect('surfaces.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE invariants (
-pg  INT, 
-q   INT,
-K2  INT,
-chi INT,
-e   INT,
-h11 INT
+kdim INT,
+pg   INT, 
+q    INT,
+K2   INT,
+chi  INT,
+e    INT,
+h11  INT
 );
 ''')
 
-entries = 0
+### Kodaira dimension = -1  --- rational surfaces
+
+rationalsurfaces = [(-1, 0, 0, 9, 1, 3, 0)]
+for n in range(2,60):
+	h11 = n+1
+	e = n+3
+	K2 = 12 - e
+	rationalsurfaces.append((-1, 0, 0, K2, e, h11, n))
+
+c.executemany("INSERT INTO invariants VALUES (?,?,?,?,?,?,?)", rationalsurfaces)
+
+### Kodaira dimension = 2   ---  a.k.a. general type
+
+kdim = 2
+
 for pg in range(0,pgmax+1):
     for q in range(0,qmax+1):
         for K2 in range(1,K2max+1):
@@ -35,12 +50,10 @@ for pg in range(0,pgmax+1):
                 continue
             if !(2*pg <= K2 + 4): # Noether
                 continue
-            query = "INSERT INTO invariants VALUES (%d,%d,%d,%d,%d,%d);" % (pg, q, K2, chi, e, h11)
+            query = "INSERT INTO invariants VALUES (%d,%d,%d,%d,%d,%d,%d);" % (kdim, pg, q, K2, chi, e, h11)
             c.execute(query)
-            #print("pg = %d, q = %d, K2 = %d, chi = %d, e = %d, h11 = %d" % (pg, q, K2, chi, e, h11))
-            entries += 1
 
-print("pg <= %d, q <= %d, K2 <= %d :: %d hits" % (pgmax, qmax, K2max, entries))
+### Kodaira
 
 c.close()
 conn.commit()
