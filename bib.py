f31f25aa8e354d470b822b975866139ef8123dfc
Johan Commelin <j.m.commelin@gmail.com>
2014-09-26 11:48:28 +0200

    Add rational surfaces to the bibliography

--- bib.py
+++ bib.py
@@ -2,16 +2,24 @@ import sqlite3
 conn = sqlite3.connect('surfaces.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE bibliography (
-pg  INT, 
-q   INT,
-K2  INT,
-chi INT,
-e   INT,
-h11 INT,
-sp  INT,
-ref TEXT
+kdim INT,
+pg   INT, 
+q    INT,
+K2   INT,
+chi  INT,
+e    INT,
+h11  INT,
+sp   INT,
+ref  TEXT
 );
 ''')
+
+
+rationalsurfaces = [(-1, 0, 0, "NULL", "NULL", "NULL", 0, 4, '''The projective plane, birational to the Hirzebruch surface $\\Sigma_{0} = \\mathbb{P}^{1} \\times \mathbb{P}^{1}$.''')]
+for n in range(2,60):
+	rationalsurfaces.append((-1, 0, 0, "NULL", "NULL", "NULL", n, 4, "The Hirzebruch surface $\\Sigma_{" + str(n) + "}$."))
+
+c.executemany("INSERT INTO bibliography VALUES (?,?,?,?,?,?,?,?,?)", rationalsurfaces)
 
 c.close()
 conn.commit()
