4aae328f4661fe63333b3896e70676dcfb89ccd1
Pieter Belmans <pieterbelmans@gmail.com>
2014-12-30 14:00:28 +0100

    Implemented smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
   // we initialise everything on Kodaira dimension -oo
   setKodairaDimension("-1");
+
+  // smooth scrolling
+  $(".scroll").click(function(e) {
+    // disable the click
+    event.preventDefault();
+
+    // compute the location for the scrolling
+    var name = this.href.split("#")[1];
+    var target = $("a[name='" + name + "']").offset().top;
+    // compensate for header
+    target -= 120;
+
+    // execute the scroll
+    $("html, body").animate({scrollTop: target}, "slow");
+  });
 });
