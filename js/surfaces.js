b27e2ffe96210791b6ac4a4cd2cd4831af10d88a
Johan Commelin <j.m.commelin@gmail.com>
2014-12-30 14:36:41 +0100

    Add surfaces of Kodaira dimension 0

--- js/surfaces.js
+++ js/surfaces.js
@@ -1,5 +1,7 @@
 var surfaces = [];
 
+// Kodaira dimension -\infty
+
 surfaces.push(
   {
     "name" : "projective plane",
@@ -33,3 +35,45 @@ for (var g = 1; g < 5; g++) {
     }
   );
 }
+
+// Kodaira dimension 0
+
+surfaces.push(
+  {
+    "name" : "Abelian surfaces",
+    "kodaira" : 0,
+    "c2" : 0,
+    "c12" : 0,
+    "h11" : 4,
+  }
+);
+
+surfaces.push(
+  {
+    "name" : "Enriques surfaces",
+    "kodaira" : 0,
+    "c2" : 12,
+    "c12" : 0,
+    "h11" : 10,
+  }
+);
+
+surfaces.push(
+  {
+    "name" : "K3 surfaces",
+    "kodaira" : 0,
+    "c2" : 24,
+    "c12" : 0,
+    "h11" : 20,
+  }
+);
+
+surfaces.push(
+  {
+    "name" : "Hyperelliptic surfaces",
+    "kodaira" : 0,
+    "c2" : 36,
+    "c12" : 0,
+    "h11" : 2,
+  }
+);
