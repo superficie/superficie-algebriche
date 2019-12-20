711e9994acba7dfb1a60283e899a91f980230a06
Pieter Belmans <pieterbelmans@gmail.com>
2019-12-20 09:59:17 +0100

    Removed debugging output

--- js/surfaces.js
+++ js/surfaces.js
@@ -210,9 +210,7 @@ var fatighenti = new Surface("Fatighenti surfaces", 2, {c12: 6, pg: 1, q: 0});
 surfaces.push(fatighenti);
 
 for (var pg = 3; pg <= 10; pg++) {
-  console.log({c12: 2*pg - 4, pg: pg, q: 0});
   var horikawa = new Surface("Horikawa surfaces", 2, {c12: 2*pg - 4, pg: pg, q: 0});
-  console.log(horikawa);
   surfaces.push(horikawa);
 }
 
